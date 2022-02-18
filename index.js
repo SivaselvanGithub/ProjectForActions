ebacd29430f21c6ef50b7446837c2ce0d559cacf
Sivaselvan Veluchamy <Sivaselvan.Veluchamy@philips.com>
2022-02-18 14:12:26 +0530

    Userprofile

---
+++ index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+	const authToken = core.getInput('token')
+	const octokit = github.getOctokit(authToken)
+
+	const orgProjectsList = []
+
+	// Get all org projects
+	const orgProjects = await octokit.rest.projects
+		.listForOrg({
+			org: github.context.repo.owner,
+		})
+		.then((result) => {
+			// Push repo ids
+			result.data.forEach((project) => {
+				orgProjectsList.push(project.id)
+			})
+		})
+		.catch((error) => {
+			core.setFailed(error.message)
+		})
+
+  core.info(`Found ${orgProjectsList.length} organization projects`)
+
+	// Set all projects to private
+	if (orgProjectsList.length > 0) {
+		orgProjectsList.forEach(async (projectId) => {
+      core.info(`Setting project ${projectId} to private...`)
+			await octokit.rest.projects.update({
+				project_id: projectId,
+				private: true,
+			})
+		})
+	}
+}
+
+run()
