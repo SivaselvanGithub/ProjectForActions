aab25867ac494316ded2ac30f214c07eef1a2997
Sivaselvan <97441452+SivaselvanVeluchamy@users.noreply.github.com>
2022-02-18 13:59:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const APIURL = 'https://api.github.com/users/'
 
+const { Octokit } = require("@octokit/action");
+
+const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+// See https://developer.github.com/v3/issues/#create-an-issue
+const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+  owner,
+  repo,
+  title: "My test issue",
+});
+console.log("Issue created: %s", data.html_url);
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
